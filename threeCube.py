d7daa6bae325bfbda6a8047644910d5419711449
geryfabrega <54220892+geryfabrega@users.noreply.github.com>
2022-04-22 11:39:47 -0400

    Update threeCube.py
    Now with X and Y axis rotation!!

--- threeCube.py
+++ threeCube.py
@@ -3,6 +3,7 @@ from bresenham import bresenham
 import time
 from curses import wrapper
 import curses
+import math
 
 
 # Update the buffer, adding text at different locations
@@ -51,7 +52,7 @@ running = True
 
 x = 0
 y = 0
-z = 0
+z = 5
 
 key = ''
 
@@ -70,21 +71,78 @@ h = 10
 """ Sprite object creation area end"""
     # this determines what objects the keyboard inputs will effect
 
+
+theta = 0
+
 finalLine = []
 
 while key != ord('q'):
 
     
 
-    vAf = [x-2, y-0.5, z + 5]
-    vBf = [x-2, y+0.5, z + 5]
-    vCf = [x-1, y+0.5, z + 5]
-    vDf = [x-1, y-0.5, z + 5]
+    # vAf = [x-2, y-0.5, z + 5]
+    # vBf = [x-2, y+0.5, z + 5]
+    # vCf = [x-1, y+0.5, z + 5]
+    # vDf = [x-1, y-0.5, z + 5]
 
-    vAb = [x-2, y-0.5, z + 6]
-    vBb = [x-2, y+0.5, z + 6]
-    vCb = [x-1, y+0.5, z + 6]
-    vDb = [x-1, y-0.5, z + 6]
+    # vAb = [x-2, y-0.5, z + 6]
+    # vBb = [x-2, y+0.5, z + 6]
+    # vCb = [x-1, y+0.5, z + 6]
+    # vDb = [x-1, y-0.5, z + 6]
+
+    vAf = [-2, -0.5, -.5]
+    vBf = [-2, +0.5, -.5]
+    vCf = [-1, +0.5, -.5]
+    vDf = [-1, -0.5, -.5]
+
+    vAb = [-2, -0.5, .5]
+    vBb = [-2, +0.5, .5]
+    vCb = [-1, +0.5, .5]
+    vDb = [-1, -0.5, .5]
+
+    frontFace = [vAb,vBb,vCb,vDb]
+    frontFace = np.array(frontFace)
+
+    backFace = [vAf,vBf,vCf,vDf]
+    backFace = np.array(backFace)
+
+    rotatex = [[1,0,0],
+        [0,math.cos(theta),-math.sin(theta)],
+        [0,math.sin(theta),math.cos(theta)]]
+
+    rotatey = [[math.cos(theta),0,math.sin(theta)],
+        [0,1,0],
+        [-math.sin(theta),0,math.cos(theta)]]
+
+    rotatex = np.array(rotatex)
+    rotatey = np.array(rotatey)
+
+    frontFace = np.dot(frontFace,rotatex)
+    backFace = np.dot(backFace,rotatex)
+
+    frontFace = np.dot(frontFace,rotatey)
+    backFace = np.dot(backFace,rotatey)
+
+    vAf = frontFace[0]
+    vAf[2] += z
+    vBf = frontFace[1]
+    vBf[2] += z
+    vCf = frontFace[2]
+    vCf[2] += z
+    vDf = frontFace[3]
+    vDf[2] += z
+
+    vAb = backFace[0]
+    vAb[2] += z
+    vBb = backFace[1]
+    vBb[2] += z
+    vCb = backFace[2]
+    vCb[2] += z 
+    vDb = backFace[3]
+    vDb[2] += z 
+
+
+
     
     pair1 = projectVertex(vAf)
     pair2 = projectVertex(vBf)
@@ -182,11 +240,15 @@ while key != ord('q'):
 
     finalLine.append(tempLine)
 
+    theta += .01
+
+
+
     screen.clear()
     for j in finalLine:
         for i in j:
             try:
-                screen.addstr(i[1],i[0],"*")
+                screen.addstr(i[1],i[0],"#")
             except:
                 pass
 
@@ -205,5 +267,7 @@ while key != ord('q'):
 
 
 curses.endwin()
+
+
 
 
